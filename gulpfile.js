9038cca7f59e1844689157346cdbbf3227bb2eb1
David <davbohr@gmail.com>
2016-06-14 15:06:26 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var prefix       = require('gulp-autoprefixer');
 var cp           = require('child_process');
 var fs           = require('fs');
 var ghPages      = require('gulp-gh-pages');
+var extract      = require('gulp-html-extract');
 var jekyll       = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages     = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
@@ -21,10 +22,10 @@ var messages     = {
 /**
  * Build the Jekyll Site
  */
- gulp.task('jekyll-build', function (done) {
+gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn( jekyll , ['build', '--config', '_config.yml'], {stdio: 'inherit'})
-    .on('close', done);
+        .on('close', done);
 });
 
 
@@ -33,13 +34,13 @@ var messages     = {
  * Rebuild Jekyll & do page reload
  */
  gulp.task('jekyll-rebuild', ['optimize-html'], function () {
-   browserSync.reload();
-});
+     browserSync.reload();
+ });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
- gulp.task('browser-sync', ['optimize-html'], function() {
+gulp.task('browser-sync', ['optimize-html'], function() {
     browserSync({
         server: {
             baseDir: '_site'
@@ -50,49 +51,42 @@ var messages     = {
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
- gulp.task('optimize-css', ['jekyll-build'], function () {
+gulp.task('optimize-css', ['jekyll-build'], function () {
     return gulp.src('_site/css/main.css')
-    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(rename('all.min.css'))
-    .pipe(gulp.dest('_site/public/css'))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest('public/css'));
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(rename('all.min.css'))
+        .pipe(gulp.dest('_site/public/css'))
+        .pipe(browserSync.reload({stream:true}))
 });
 
 
- gulp.task('optimize-js', ['optimize-css'], function() {
-  return gulp.src('js/*.js')
-  .pipe(concat('all.min.js'))
-  .pipe(gulp.dest('_site/public/js'))
-  .pipe(browserSync.reload({stream:true}))
-  .pipe(gulp.dest('public/js'));
+gulp.task('optimize-js', ['optimize-css'], function() {
+     return gulp;
 });
 
- gulp.task('optimize-html', ['optimize-js'], function() {
-    return gulp.src('_site/**/*.html')
-    .pipe(gulp.dest('_site/'));
+gulp.task('optimize-html', ['optimize-js'], function() {
+    return gulp;
 });
 
 /**
  * Watch html/md files, run jekyll & reload BrowserSync
  */
- gulp.task('watch', function () {
+gulp.task('watch', function () {
     gulp.watch(['_sass/*.scss',
-        'css/*.scss',
-        'js/*.js',
-        '*.html',
-        '_layouts/*.html',
-        '_includes/*.html',
-        '_includes/*.svg',
-        '_posts/*'], ['jekyll-rebuild']);
+                'css/*.scss',
+                'js/*.js',
+                '*.html',
+                '_layouts/*.html',
+                '_includes/*.html',
+                '_includes/*.svg',
+                '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
- gulp.task('default', ['browser-sync', 'watch']);
-
+gulp.task('default', ['browser-sync', 'watch']);
 
 
 
@@ -101,55 +95,55 @@ var messages     = {
 */
 gulp.task('jekyll-build-prod', function (done) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build', '--config', '_config.prod.yml'], {stdio: 'inherit'})
-    .on('close', done);
+    return cp.spawn( jekyll , ['build', '--config', '_config.yml,_config.prod.yml'], {stdio: 'inherit'})
+        .on('close', done);
 });
 gulp.task('optimize-css-prod', ['jekyll-build-prod'], function () {
     return gulp.src('_site/css/main.css')
-    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-       .pipe(uncss({
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(uncss({
            html: ['_site/**/*.html'],
            ignore: []
-       })) // too many complications when use in conjunction with Prism.js   
-       .pipe(rename('all.min.css'))
-       .pipe(minifyCSS())
-       .pipe(gulp.dest('_site/public/css'))
-       .pipe(browserSync.reload({stream:true}))
-       .pipe(gulp.dest('public/css'))
-       .pipe(notify({ message: 'CSS-PROD task complete' }));
-   });
+       }))
+        .pipe(rename('all.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('_site/public/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(notify({ message: 'CSS-PROD task complete' }));
+});
 gulp.task('optimize-js-prod', ['optimize-css-prod'], function() {
-  return gulp.src('js/*.js')
-  .pipe(concat('all.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('_site/public/js'))
-  .pipe(browserSync.reload({stream:true}))
-  .pipe(gulp.dest('public/js'))
-  .pipe(notify({ message: 'JS-PROD task complete' }));
+  return gulp
+            .src("_site/index.html")
+            .pipe(extract())
+            .pipe(concat('all.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('_site/public/js'))
+            .pipe(notify({ message: 'JS-PROD task complete' }));
 });
 gulp.task('optimize-html-prod', ['optimize-js-prod'], function() {
     return gulp.src('_site/**/*.html')
+
+        .pipe(replace(/<link rel=\"stylesheet\" href=\"\/public\/css\/all.min.css\"[^>]*>/, function(s) {
+            var style = fs.readFileSync('_site/public/css/all.min.css', 'utf8');
+            return '<style>' + style + '</style>';
+        }))
+        .pipe(replace(/<!--startjs-->[^]+<!--endjs-->/, function(s) {
+            var js_script = fs.readFileSync('_site/public/js/all.min.js', 'utf8');
+            return '<script type="text/javascript">' + js_script + '</script>';
+        }))
     .pipe(minifyHTML({
-        quotes: true
+      quotes: true
     }))
-    .pipe(replace(/<link rel=\"stylesheet\" href=\"\/public\/css\/all.min.css\"[^>]*>/, function(s) {
-        var style = fs.readFileSync('_site/public/css/all.min.css', 'utf8');
-        return '<style>' + style + '</style>';
-    }))
-    .pipe(replace(/<script src=\"\/public\/js\/all.min.js\"[^>]*><\/script>/, function(s) {
-        var js_script = fs.readFileSync('_site/public/js/all.min.js', 'utf8');
-        return '<script type="text/javascript">' + js_script + '</script>';
-    }))
-    .pipe(gulp.dest('_site/'))
+        .pipe(gulp.dest('_site/'))
     .pipe(notify({ message: 'HTML-PROD task complete' }));
 });
 gulp.task('push-to-gh-pages', ['optimize-html-prod'], function() {
   return gulp.src('./_site/**/*')
-  .pipe(ghPages());
+    .pipe(ghPages());
 });
 gulp.task('deploy',
   ['jekyll-build-prod',
-  'optimize-css-prod',
-  'optimize-js-prod',
-  'optimize-html-prod',
-  'push-to-gh-pages']);
+    'optimize-css-prod',
+    'optimize-js-prod',
+    'optimize-html-prod',
+    'push-to-gh-pages']);
